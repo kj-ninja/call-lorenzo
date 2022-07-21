24f4a1c741dbdb68baf41f2e950d13e112ded0f1
kjakubiak <rafalek3007>
2022-07-21 20:49:02 +0200

    added styled components lib also some basic global styles config

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
+import GlobalStyle from "./styles";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primaryColor: '#002541',
+    secondaryColor: '#64809D',
+    white: "#ffffff"
+  },
+
+  fontFamily: {
+    primaryFont: '"Roboto", "sans-serif"',
+  },
+}
+
+export default theme;
