d8e64903fc6650e86da71e6f79d333185b9d588f
JaCoWymowny <jacekkombo@gmail.com>
2022-10-06 20:11:12 +0200

    add animation to whatIsOurOffer section

--- src/components/Tile/index.tsx
+++ src/components/Tile/index.tsx
@@ -13,17 +13,26 @@ interface Props {
 
 const Tile: FC<Props> = (tile) => {
   const [show, setShow] = useState(false);
+  const [fadeOutAnimation, setFadeOutAnimation] = useState(false);
 
   const { toggleText } = content.WhatIsOurOffer;
 
   const toggleHandler = () => {
-    setShow(!show);
+    if (show) {
+      setFadeOutAnimation(!fadeOutAnimation)
+      setTimeout(() => {
+        setShow(!show);
+      }, 300);
+    } else {
+      setShow(!show);
+      setFadeOutAnimation(!fadeOutAnimation)
+    }
   };
 
   return (
     <Fragment>
       <TileWrapper>
-        <StyledTile className="tile hover-effect" onClick={toggleHandler} >
+        <StyledTile className="tile hover-effect" onClick={toggleHandler}>
           <div className='tile-text-wrapper'>
             <span className='tile-order'>
               {tile.tile.tileOrder}
@@ -31,30 +40,28 @@ const Tile: FC<Props> = (tile) => {
             <span className='tile-title'>
               {tile.tile.tileTitle}
             </span>
-
           </div>
           <span className='tile-toggle'>
               {!show ? toggleText.show : toggleText.hide}
             </span>
         </StyledTile>
         {
-          show ?
-            <StyledDescription>
-              <div className='first-paragraph'>
-                {tile.tile.tileText}
-              </div>
-              <div className='second-paragraph'>
-                {tile.tile.tileDescription}
-              </div>
-              {
-                tile.tile.tileBoldText ?
-                  <div className='third-paragraph'>
-                    {tile.tile.tileBoldText}
-                  </div>
-                  : null
-              }
-            </StyledDescription>
-            : null
+          show &&
+          <StyledDescription toggler={show} className={`${fadeOutAnimation ? "" : "animation"}`}>
+            <div className='first-paragraph'>
+              {tile.tile.tileText}
+            </div>
+            <div className='second-paragraph'>
+              {tile.tile.tileDescription}
+            </div>
+            {
+              tile.tile.tileBoldText ?
+                <div className='third-paragraph'>
+                  {tile.tile.tileBoldText}
+                </div>
+                : null
+            }
+          </StyledDescription>
         }
       </TileWrapper>
     </Fragment>
